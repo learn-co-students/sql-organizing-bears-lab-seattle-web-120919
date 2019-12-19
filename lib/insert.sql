765b17f44526e3dbf245c8c04a9e6d4876195f2d
aldenweaver <aldenweavercodes@gmail.com>
2019-12-19 09:43:02 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,39 @@
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Mr. Chocolate", 2, "Male", "Brown", "Nice", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Rowdy", 2, "Male", "Brown", "Mean", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Tabitha", 5, "Female", "Brown", "Mean", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Sergeant Brown", 5, "Male", "Brown", "Nice", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Melissa", 4, "Female", "Brown", "Nice", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Grinch", 3, "Male", "Black", "Mean", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        ("Wendy", 1, "Female", "Brown", "Nice", true);
+
+INSERT INTO bears 
+        (name, age, gender, color, temperament, alive) 
+    VALUES
+        (NULL, 2, "Male", "White", "Mean", false);
